9c016cf5474d92517717885a23f3011c52a9d024
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-01 19:55:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  k = nil
-  v = nil
+  k = []
+  v = []
     name_hash.collect do |key, value|
       if value < v
         k += 1
