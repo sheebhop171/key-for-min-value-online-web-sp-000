0be136d6216b404e3420259e63df8beb1969f3d3
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-01 19:56:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   k = []
   v = []
     name_hash.collect do |key, value|
-      if v[1] < v[2]
-        k += 1
+      if value < v
+        v += 1
       else
         nil
       end
